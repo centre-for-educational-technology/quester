4d41fa6b81165ca556f0843b0e581b81c0016b38
Aili Mets <ailimets@Ailis-MacBook-Pro-2.local>
2022-05-10 10:26:25 +0300

    references #1, for using permissions in vue pages

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -24,3 +24,9 @@
         @endenv
     </body>
 </html>
+<script type="text/javascript">
+    window.Laravel = {
+        csrfToken: "{{ csrf_token() }}",
+        jsPermissions: {!! auth()->check()?auth()->user()->jsPermissions():0 !!}
+    }
+</script>
