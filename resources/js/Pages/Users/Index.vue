711887bbf06a6d7b2e5c337976afc9ca53c78093
Pankaj Chejara <pankajchejara23@gmail.com>
2022-08-26 16:07:00 +0530

    styling edit link on users list

--- resources/js/Pages/Users/Index.vue
+++ resources/js/Pages/Users/Index.vue
@@ -30,7 +30,15 @@
                                 </div>
                             </td>
                             <td class="uppercase text-sm">
-                                <a :href="'/users/'+user.id+'/edit'">Edit</a>
+                                <a :href="'/users/'+user.id+'/edit'">
+                                        <el-tooltip content="Edit" placement="top">
+                                            <el-button class=" hover:text-indigo-900 ml-2">
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="text-indigo-600 w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                                </svg>
+                                            </el-button>
+                                        </el-tooltip>
+                                </a>
                             </td>
                         </tr>
                         </tbody>
