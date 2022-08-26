eb1d13483ec65f66d45151f3e872ec8a6c831518
Pankaj Chejara <pankajchejara23@gmail.com>
2022-08-26 17:06:06 +0530

    display delete for admin only

--- resources/js/Pages/Results/Index.vue
+++ resources/js/Pages/Results/Index.vue
@@ -18,6 +18,7 @@
                         <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Start Date</th>
                         <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">End Date</th>
                         <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Responses</th>
+                        <th v-if="admin" scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"></th>
                         </thead>
                         <tbody class="divide-y divide-gray-200 bg-white">
                         <tr v-for="(questionnaire, index) in questionnaires" class="border m-1 border-gray-200">
@@ -32,6 +33,17 @@
                             <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ formatDateTime(questionnaire.end_time) }}</td>
                             <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                                 {{questionnaire.respondents.length}}
+                            </td>
+                            <td v-if="admin" class="uppercase text-sm text-gray-500">
+                                    <a v-on:click="showAlertConfirm(questionnaire.id)">
+                                        <el-tooltip content="Delete" placement="top">
+                                            <el-button class=" hover:text-red-800 ml-2">
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6  text-red-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                                </svg>
+                                            </el-button>
+                                        </el-tooltip>
+                                    </a>
                             </td>
                         </tr>
                         </tbody>
@@ -56,10 +68,43 @@ export default defineComponent({
         NavLink,
         JetButton,
     },
-    props: ['questionnaires'],
+    props: ['questionnaires','admin'],
+    data() {
+        return {
+            base_url: window.location.origin,
+            result_id: '',
+            questionnaire_code: '',
+        }
+    },
     methods: {
         formatDateTime(datetime)  {
             return moment.utc(datetime).local().format("DD.MM.YYYY H:mm");
+        },
+        showAlertConfirm(id){
+            this.$swal({
+              title: 'Are you sure?',
+              text: "This will permanently delete the result. You won't be able to revert this!",
+              type: 'warning',
+              icon: 'warning',
+              showCancelButton: true,
+              confirmButtonColor: '#991b1b',
+              cancelButtonColor: '#3085d6',
+              confirmButtonText: 'Yes, delete it!'
+
+            }).then((result) => {
+              if (result.value) {
+                axios.get('/results/'+id+'/delete')
+                    .then(res => {
+                        window.location.href = this.base_url + "/results"
+                    }).catch(err => {
+                    console.log(err)
+                })
+
+              }
+              else {
+                return false;
+              }
+            });
         }
     },
 })
